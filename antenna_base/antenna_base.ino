8f0da8368a7f7910b3796de42dede528e9ab31f2
Adam Silverman <32516544+ajs0429@users.noreply.github.com>
2018-12-27 19:30:38 -0500

    Add files via upload

--- antenna_base/antenna_base.ino
+++ antenna_base/antenna_base.ino
@@ -1,88 +1,94 @@
 //Import required files
 #include <ros.h>
-#include <std_msgs/Byte.h>
-#include <std_msgs/Bool.h>
-#include <std_msgs/Int16.h>
 #include <std_msgs/Float32.h>
 #include <AltSoftSerial.h>
 #include <AltSSPololuQik.h>
+#include <math.h> 
 
 //AltSoftSerial uses Pin 8 for Rx and Pin 9 for Tx, and that is unchangeable because hardware interrupts. PWM Pin 10 cannot be used.
 //Connect Arduino Rx to driver Tx , arduino Tx to driver Rx
 
 const byte resetPin = 4;
-bool timeout = false;
-float imu = 0;
+float base_hdg = 0.0;
 const byte SPEED = 60;
+const float ERR = 0.5;
 
 PololuQik2s12v10 arm(resetPin);
 
 //Create a ROS node-handler to handle ROS stuff
 ros::NodeHandle  nh;
 
-std_msgs::Byte error_msg;
 
-ros::Publisher error("error", &error_msg);
+std_msgs::Float32 chatter_msg;
+ros::Publisher chatter("chatter", &chatter_msg);
 
-//Declare a reset function
-void(* resetFunc) (void) = 0;
+std_msgs::Float32 base_msg;
+ros::Publisher base("base_hdg", &base_msg);
 
-void setTimeOut(const std_msgs::Bool &timeout_data) {
-  timeout = timeout_data.data;
-}
+std_msgs::Float32 rover_msg;
+ros::Publisher rover("rover_hdg", &rover_msg);
 
 void moveJoint(const std_msgs::Float32 &a) {
-  float deg = a.data;
-  if (imu < deg) {
-    while (imu < deg && abs(imu - deg) < 0.5) {
-      arm.setM0Speed(SPEED);
+  float rover_hdg = a.data;
+  rover_msg.data = rover_hdg;
+  rover.publish(&rover_msg);
+  base_msg.data = base_hdg;
+  base.publish(&base_msg);
+
+  float diff = getDifference(rover_hdg, base_hdg);
+
+  if (diff > 0) {
+    chatter_msg.data = 1;
+    while (abs(base_hdg - rover_hdg) > ERR) {
+      arm.setM0Speed(SPEED); // clockwise
     }
     arm.setM0Speed(0);
-  } else if (imu > deg) {
-    while (imu > deg && abs(imu - deg) < 0.5) {
-      arm.setM0Speed(-SPEED);
+  } else if (diff < 0) {
+    chatter_msg.data = -1;
+    while (abs(base_hdg - rover_hdg) > ERR) {
+      arm.setM0Speed(-SPEED); // counter-clockwise
     }
     arm.setM0Speed(0);
   } else {
+    chatter_msg.data = -10;
     arm.setM0Speed(0);
   }
+  //arm.setM0Speed(SPEED);
 }
 
 void getAntennaHeading(const std_msgs::Float32 &i) {
-  imu = i.data;
+  base_hdg = i.data;
 }
 
 //Setup subscribers
 
-ros::Subscriber<std_msgs::Float32> sub_degrees("base_bearing", &moveJoint);
-ros::Subscriber<std_msgs::Bool> sub_timeout("timeout", &setTimeOut);
-ros::Subscriber<std_msgs::Float32> sub_imu("IMU_base", &getAntennaHeading);
+ros::Subscriber<std_msgs::Float32> sub_rover_hdg("base_bearing", &moveJoint);
+ros::Subscriber<std_msgs::Float32> sub_base_hdg("IMU_rover", &getAntennaHeading);
 
 
 void setup()
 {
   //Initiate the node & subscribers
   nh.initNode();
-  nh.subscribe(sub_degrees);
-  nh.subscribe(sub_timeout);
-  nh.subscribe(sub_imu);
+  nh.subscribe(sub_rover_hdg);
+  nh.subscribe(sub_base_hdg);
+  nh.advertise(chatter);
+  nh.advertise(rover);
+  nh.advertise(base);
   //Initialize serial comms with the driver
   arm.init();
   delay(10);
+  arm.setM0Speed(0);
   arm.setM1Speed(0);
 }
 
 
 void loop()
-
 {
-  if (!timeout) {
-    nh.spinOnce();
-  } else {
-    arm.setM0Speed(0);
-    arm.setM1Speed(0);
-    delay(1000);
-    resetFunc();
-  }
+  nh.spinOnce();
+  chatter.publish(&chatter_msg);
+}
 
+float getDifference(float a1, float a2) {
+  return fmod((fmod(a1 - a2, 360) + 540), 360) - 180;
 }
